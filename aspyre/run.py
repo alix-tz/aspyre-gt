d8a03bb0e6a62ed02ff440fa26e918a36972fbe5
alix.chague@inria.fr <alix.chague@inria.fr>
2021-02-11 22:15:56 +0100

    implementing classes for better modularity

--- aspyre/run.py
+++ aspyre/run.py
@@ -10,23 +10,37 @@ date: 26/08/2020
 import argparse
 import os
 
-from aspyrelib import aspyre
+from aspyrelib.aspyre import (AspyreArgs, TkbToEs)
 from aspyrelib.utils import utils as utils
 
 
-parser = argparse.ArgumentParser(description="Aspyre is a program transforming ALTO XML files exported from Transkribus (ALTO 2.x) to make them compatible with eScriptorium (ALTO 4.x)")
-parser.add_argument('-i', '--source', action='store', nargs=1, default=[False], help='Location of the Transkirbus Export directory')
-parser.add_argument('-o', '--destination', action='store', nargs=1, default=[False], help='Location where resulting files should be stored (path to an existing directory)')
-parser.add_argument('-t', '--talktome', action='store_true', help="Will display highlighted messages if activated")
-parser.add_argument('-m', '--mode', action='store', nargs=1, default='default', help="default|test")
+parser = argparse.ArgumentParser(description="Aspyre is a program transforming files to make them compatible" +
+                                             "with eScriptorium Import XML module")
+parser.add_argument('-i', '--source', action='store', nargs=1, required=True,
+                    help='Location of the source files')
+parser.add_argument('-sc', '--scenario', action='store', nargs=1, required=True,
+                    help='Determines which transformation scenario will be applied (transkribus|limb|fineread)')
+parser.add_argument('-o', '--destination', action='store', nargs=1, default=[False],
+                    help='Location where resulting files should be stored (path to an existing directory)')
+parser.add_argument('-t', '--talktome', action='store_true',
+                    help="Will display highlighted messages if activated")
+parser.add_argument('-m', '--mode', action='store', nargs=1, default='default',
+                    help="default|test")
 args = vars(parser.parse_args())
 
 # start main task
 if args['mode'].lower() == 'test':
     pass
 elif args['mode'].lower() == 'default':
-    aspyre_report = aspyre.main(src=args['source'][0], dest=args['destination'][0], talktome=args['talktome'])
-    utils.report(f"report:{aspyre_report}")
+    aspyre_args = AspyreArgs(scenario=args['scenario'][0], source=args['source'][0],
+                             destination=args['destination'][0], talkative=args['talktome'])
+    if aspyre_args.proceed():
+        if aspyre_args.scenario == 'transkribus':
+            transfo = TkbToEs(aspyre_args)
+    if args['talktome']:
+        utils.report(f"Displaying execution log (status: {aspyre_args.execution_status}):", "I")
+        for entry in aspyre_args.log:
+            utils.report(entry, "I")
 else:
     utils.report(f"{args['mode']} is not a valid mode", "E")
 
